c414f3467ba742a5b008d94c2b9ad04bfd3f9e8e
DataScience <43822230+HosseinKhandani@users.noreply.github.com>
2025-01-22 13:41:06 +0330

    Update app_balanced_1102.py

--- app_balanced_1102.py
+++ app_balanced_1102.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import pickle #as pickle
+import pickle4 as pickle
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import xgboost as xgb
 
